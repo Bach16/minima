febfef2b8cee0fc43b8bc7f90e37c68f0e652296
Bach16 <gwyn56717@gmail.com>
2023-11-19 22:40:57 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
