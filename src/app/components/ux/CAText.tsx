48f8a326b1f136cc077af51c881420a08ec13da9
Bach16 <gwyn56717@gmail.com>
2023-11-21 20:14:48 -0500

    Update CAText.tsx

--- src/app/components/ux/CAText.tsx
+++ src/app/components/ux/CAText.tsx
@@ -4,14 +4,13 @@ const CAText = () => {
   return (
     <div className="flex flex-col justify-between h-[12rem]  ">
       <p className="w-[18rem] sm:w-[26rem] text-[0.8rem] sm:text-[1rem] font-light">
-        Minima es la agencia que te cautiva, te emociona y te apasiona, es tu
-        mundo Telemundo, un mundo de emoción es Telemundo te hace reír te
-        apasiona te emociona, es tu mundo Telemundo un mundo de emoción es
-        Telemundo
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
       </p>
       <div className="w-[15rem]">
-
-      <CAButton content="Mira nuestros proyectos"/>
+        <CAButton content="Mira nuestros proyectos" />
       </div>
     </div>
   );
