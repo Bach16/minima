3615da1a966052ef51623e5f16751a3143e03b43
Bach16 <gwyn56717@gmail.com>
2023-12-04 06:21:06 -0500

    Update Column.tsx

--- src/app/components/Column.tsx
+++ src/app/components/Column.tsx
@@ -12,10 +12,10 @@ const Column:FC<Props> = ( props ) => {
 
     const {y,top} = props
   return (
-    <motion.div style={{y,"top":top}} className="w-[25%] h-[200%] flex flex-col gap-[2vw] min-w-[150px] sm:min-w-[250px] relative">
+    <motion.div style={{y,"top":top}} className="w-[100%] h-[100%] flex flex-col gap-[2vw] min-w-[54rem] sm:min-w-[54rem] relative">
       {props.imgs.map((src: string, index: number) => {
         return (
-          <div key={index} className="w-[100%] max-h-[350px] h-[150%] relative rounded-lg overflow-hidden">
+          <div key={index} className="w-[100%] max-h-[30rem] h-[100%] relative rounded-lg overflow-hidden">
             <Image className="object-cover"  src={src} fill alt="projects img" />
           </div>
         );
