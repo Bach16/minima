377534c42aa9aa9a0ff51f094609d52f199e4256
Bach16 <gwyn56717@gmail.com>
2023-12-04 06:21:25 -0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 import { IoIosMenu } from "react-icons/io";
 
@@ -6,17 +7,21 @@ const Navbar = () => {
   return (
     <nav className="flex h-[5rem] justify-center">
       <div className="flex justify-between items-center w-[90%]">
-        <Image
-          src="/Artboard38.webp"
-          width={128.825}
-          height={31.08}
-          alt="Minima Logo"
-        />
-        <div className=" hidden sm:flex font-light w-[25%] justify-between">
-          <p>Proyectos</p>
-          <p>Contacto</p>
-          <p>Nosotros</p>
-        </div>
+        <Link href={"/"}>
+          <Image
+            src="/Artboard38.webp"
+            width={128.825}
+            height={31.08}
+            alt="Minima Logo"
+          />
+        </Link>
+        <ul className=" hidden sm:flex font-light w-[25%] justify-between">
+          <Link href={"/projects"}>
+            <li>Proyectos</li>
+          </Link>
+          <li>Contacto</li>
+          <li>Nosotros</li>
+        </ul>
         <button className="flex sm:hidden">
           <IoIosMenu className="w-[2rem] h-[2rem]" />
         </button>
