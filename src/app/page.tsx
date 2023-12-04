89bd5a04f46a45eb219823d1478c5a87964d839c
Bach16 <gwyn56717@gmail.com>
2023-12-04 06:21:54 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,24 @@ import {
   Bebas_Neue,
   Oswald,
 } from "next/font/google";
-import { CAButton, CAText, Column, Navbar } from "./components";
+import {
+  ButtonFooter,
+  CAButton,
+  CAText,
+  Card,
+  Column,
+  FooterContainer,
+  Navbar,
+  SimpleProjectContainer,
+  SocialItems,
+  TextCAContainer,
+  Title,
+  WideProjects,
+} from "./components";
 import { useEffect, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
 import { useScroll, useTransform, motion } from "framer-motion";
-import useDimension from "./assets/useDimension";
+import useDimension from "../assets/useDimension";
 
 const oswald = Oswald({
   subsets: ["latin"],
@@ -27,16 +40,7 @@ const Alumni = Alumni_Sans({
   display: "swap",
   weight: ["700"],
 });
-const images = [
-  "/CCLP.png",
-  "/aMedida.png",
-  "/3.png",
-  "/eventoss.png",
-  "/5.png",
-  "/6.png",
-  "/7.png",
-  "/8.png",
-];
+const images = ["/CCLP.png", "/DulcinaLanding1.png", "/vangSA.png"];
 
 export default function Home() {
   const cont = useRef(null);
@@ -50,11 +54,11 @@ export default function Home() {
 
   const y = useTransform(scrollYProgress, [0.2, 1.2], [0, height * 2]);
   const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 3.7]);
   const y4 = useTransform(
     scrollYProgress,
     [0, 1],
-    [0, height * (height < 700 ? 2.5 : 3)]
+    [0, height * (height < 700 ? 2.5 : 3.1)]
   );
 
   useEffect(() => {
@@ -69,6 +73,8 @@ export default function Home() {
   }, []);
 
   return (
+    <>
+    
     <main className="h-[300rem]   text-[#ffffff]  bg-no-repeat bg-fixed ">
       <div className="min-h-screen">
         <Navbar />
@@ -97,11 +103,11 @@ export default function Home() {
             </h1>
           </div>
           <div className=" mt-[2rem] sm:mt-0 sm:absolute sm:right-[4rem] sm:bottom-[10rem]">
-            <CAText />
+            <TextCAContainer />
           </div>
         </div>
-        <div className="h-[95vh] flex flex-col items-center ">
-          <div className="w-[100%] h-[50%]" />
+        <div className="w-[100%] h-[30vh]" />
+        {/*  <div className="h-[95vh] flex flex-col items-center ">
           <div className="h-[30%] relative flex flex-col justify-between items-center z-50">
             <h2 className="sm:w-[55%] w-[90%] text-center font-extrabold sm:leading-[4.5rem] leading-[2rem] text-[2rem] sm:text-[4rem]">
               Diseños creados a la medida de tu negocio
@@ -111,65 +117,72 @@ export default function Home() {
               coaching, and content your business needs to grow and thrive.
             </p>
           </div>
-        </div>
-        <div className=" z-0 h-[300vh] absolute w-[100%] top-[53rem] left-[0] bg-gradient-to-tr from-[#000000] via-black to-[#2F2F2F] skew-y-[-15deg]" />
-        <div className="flex h-[100vh] sm:h-[175vh] flex-col sm:flex-row w-[100%]">
+        </div> */}
+        <div className="flex h-[100vh] sm:h-[200vh] overflow-hidden flex-col sm:flex-row w-[100%]">
           <div
             ref={cont}
-            className=" h-[100%] w-[100%] sm:w-[50%] overflow-hidden bg-[#131313] flex gap-[2vw] p-[2vw] box-border"
+            className=" h-[100%] w-[100%]  bg-[#131313] flex gap-[2vw] justify-center "
           >
-            {/* <Column imgs={[images[2], images[6], images[7]]} y={y} top={"-45%"} />
-          <Column
-            imgs={[images[3], images[4], images[7]]}
-            y={y2}
-            top={"-95%"}
-          /> */}
+            <Column
+              key={"0"}
+              imgs={[images[2], images[1], images[0]]}
+              y={y4}
+              top={"-75%"}
+            />
             <Column
               key={"1"}
-              imgs={[images[4], images[2], images[7]]}
+              imgs={[images[2], images[0], images[1]]}
               y={y3}
-              top={height < 700 ? "-80%" : "-28%"}
+              top={"-95%"}
             />
             <Column
               key={"2"}
-              imgs={[images[3], images[0], images[4]]}
+              imgs={[images[2], images[1], images[0]]}
               y={y4}
-              top={height < 700 ? "-162%" : "-75%"}
+              top={"-75%"}
             />
           </div>
-          <div className="h-[100%] hidden sm:flex justify-center items-center w-[50%] overflow-hidden">
-            <motion.div
-              style={{ y, top: "-40%" }}
-              className="h-[12rem] hidden sm:flex w-[100%] relative flex-col justify-between items-center z-50"
+        </div>
+        <div className="sm:flex justify-center items-center w-[100%] sm:h-[90vh]">
+          <div className=" flex w-[90%] h-[55%]">
+            <div className=" flex justify-start items-start w-[50%] h-[100%]">
+              <p className="text-[#ffffff] font-semibold text-[1.2rem]">
+                Featured Projects
+              </p>
+            </div>
+            <div
+              className={`flex flex-col relative top-[0rem] left-[5.5rem] font-black text-[#ffffff] text-[9rem] leading-[8.5rem]  ${anton.className}`}
             >
-              <div className="w-[100%] flex flex-col justify-between items-center">
-                <h2 className="w-[80%] text-center font-extrabold leading-[4.5rem] text-[4rem]">
-                  Nuestro trabajo
-                </h2>
-                <p className="w-[45%] text-center">
-                  Unlock your potential with a Membership and get the community.
-                </p>
-              </div>
-              <div className="w-[13.5rem]">
-                <CAButton content={"Cotiza tu pagina web"} />
-              </div>
-            </motion.div>
+              <p
+                style={{
+                  WebkitTextFillColor: "transparent",
+                  WebkitTextStrokeWidth: "0.6px",
+                }}
+                className={``}
+              >
+                PROJECTS
+              </p>
+              <p className="">PROJECTS</p>
+              <p
+                style={{
+                  WebkitTextFillColor: "transparent",
+                  WebkitTextStrokeWidth: "0.6px",
+                }}
+                className=""
+              >
+                PROJECTS
+              </p>
+            </div>
           </div>
         </div>
-        <div className="h-[10rem] sm:hidden mt-[5rem] flex w-[100%] relative flex-col justify-between items-center z-50">
-          <div className="w-[95%] flex flex-col justify-between items-center">
-            <h2 className="w-[100%]  text-center font-extrabold leading-[2rem] text-[2rem] ">
-              Nuestro trabajo
-            </h2>
-            <p className="w-[100%] text-center">
-              Unlock your potential with a Membership and get the community.
-            </p>
-          </div>
-          <div className="w-[13.5rem]">
-            <CAButton content={"Cotiza tu pagina web"} />
-          </div>
-        </div>
+        
+        <WideProjects image={images[1]} index={"01"} title="Dulcina" />
+        <WideProjects image={images[0]} index={"02"} title="Casual Couture" justify="rigth"/>
+        
+        
       </div>
     </main>
+    <FooterContainer/>
+  </>
   );
 }
