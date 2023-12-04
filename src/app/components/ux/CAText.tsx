93a1f7823b084283e1e12b1a1f4e6d4006ae3793
Bach16 <gwyn56717@gmail.com>
2023-12-04 06:21:43 -0500

    Update CAText.tsx

--- src/app/components/ux/CAText.tsx
+++ src/app/components/ux/CAText.tsx
@@ -2,17 +2,12 @@ import { CAButton } from ".";
 
 const CAText = () => {
   return (
-    <div className="flex flex-col justify-between h-[12rem]  ">
       <p className="w-[18rem] sm:w-[26rem] text-[0.8rem] sm:text-[1rem] font-light">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </p>
-      <div className="w-[15rem]">
-        <CAButton content="Mira nuestros proyectos" />
-      </div>
-    </div>
   );
 };
 
