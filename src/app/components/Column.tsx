b29f219fc5d29a3a70b43f69d742dd18f9d73bd0
Bach16 <gwyn56717@gmail.com>
2023-12-11 07:05:24 -0500

    Update Column.tsx

--- src/app/components/Column.tsx
+++ src/app/components/Column.tsx
@@ -12,7 +12,7 @@ const Column:FC<Props> = ( props ) => {
 
     const {y,top} = props
   return (
-    <motion.div style={{y,"top":top}} className="w-[100%] h-[100%] flex flex-col gap-[2vw] min-w-[54rem] sm:min-w-[54rem] relative">
+    <motion.div style={{y,"top":top}} className="w-[100%] h-[100%] flex flex-col gap-[2vw] min-h-[29rem] min-w-[16rem] sm:min-w-[54rem] relative">
       {props.imgs.map((src: string, index: number) => {
         return (
           <div key={index} className="w-[100%] max-h-[30rem] h-[100%] relative rounded-lg overflow-hidden">
