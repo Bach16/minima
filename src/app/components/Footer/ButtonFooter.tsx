2dd41267180f5d70284ec41e2195396bf7ad0804
Bach16 <gwyn56717@gmail.com>
2023-12-11 07:05:28 -0500

    Update ButtonFooter.tsx

--- src/app/components/Footer/ButtonFooter.tsx
+++ src/app/components/Footer/ButtonFooter.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ButtonFooter: FC<Props> = ({text}) => {
   return (
-    <div className="bg-[#ffffff] border-[1px] text-[1.1rem] w-[40%] border-[#000000] border-solid flex items-center justify-center">
+    <div className="bg-[#ffffff] border-[1px] text-[1.1rem] w-[100%] h-[40%] sm:h-[100%] sm:w-[40%] border-[#000000] border-solid flex items-center justify-center">
         <p className="font-semibold text-[#000000]">
             {text}
         </p>
