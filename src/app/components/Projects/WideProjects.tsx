1a7a67ef9b3a9b4bd17a010615c65972780031b1
Bach16 <gwyn56717@gmail.com>
2023-12-11 07:05:44 -0500

    Update WideProjects.tsx

--- src/app/components/Projects/WideProjects.tsx
+++ src/app/components/Projects/WideProjects.tsx
@@ -11,12 +11,12 @@ interface Props {
 
 const WideProjects: FC<Props> = ({image,index,title,justify}) => {
   return (
-    <div className="h-[20rem] w-[100%] flex justify-center items-center mb-[20rem] ">
-      <div className={` ${justify=="rigth"?"flex-row-reverse":""} h-[100%] w-[90%] flex justify-center`}>
+    <div className="h-[20rem] w-[100%] flex justify-center items-center mb-[7rem] sm:mb-[20rem] ">
+      <div className={` ${justify=="rigth"?"sm:flex-row-reverse":"sm:flex-row"} h-[100%] w-[90%] flex justify-center flex-col`}>
         <Card image={image} />
         <div className="w-[15%]" />
-        <div className={`${justify=="rigth"?"items-end text-right":""} flex w-[45%] flex-col justify-between h-[60%]`}>
-          <Title index={index} title={title} />
+        <div className={`${justify=="rigth"?"sm:items-end sm:text-right":""} flex sm:w-[45%] flex-col justify-between h-[60%]`}>
+          <Title index={index} title={title} /> 
           <CAText />
         </div>
       </div>
