5ecdce8941ce9075bb81d8765caa5a51fc62a267
Bach16 <gwyn56717@gmail.com>
2023-12-14 06:27:43 -0500

    Update Card.tsx

--- src/app/components/Projects/Card.tsx
+++ src/app/components/Projects/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Card: FC<Props> = ({ image, bgColor }) => {
   return (
-    <div className={`${bgColor?`h-[22rem] w-[36rem] ${bgColor}` :""}   flex justify-center items-center `}>
+    <div className={`${bgColor?`h-[22rem] w-[36rem] ${bgColor} justify-center` :"justify-start"}   flex items-center `}>
       <div className={`${bgColor?`sm:h-[16.5rem] sm:w-[30rem]` :"sm:h-[19rem] sm:w-[34.5rem] h-[10rem] w-[20rem]"}  relative`}>
         <Image src={image} alt="Project Image" fill />
       </div>
