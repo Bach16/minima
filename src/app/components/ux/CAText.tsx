48ae1cd1e7eff39b99a65f26f34d3d8a746b1b80
Bach16 <gwyn56717@gmail.com>
2023-12-14 07:10:08 -0500

    Update CAText.tsx

--- src/app/components/ux/CAText.tsx
+++ src/app/components/ux/CAText.tsx
@@ -2,7 +2,7 @@ import { CAButton } from ".";
 
 const CAText = () => {
   return (
-      <p className="w-[18rem] sm:w-[26rem] text-[0.8rem] sm:text-[1rem] font-light">
+      <p className="w-[100%] sm:w-[26rem] text-[0.8rem] sm:text-[1rem] font-light">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
