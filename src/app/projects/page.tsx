7c9cf4fa3a2479c2d999ceecf8844be981fdaf26
Bach16 <gwyn56717@gmail.com>
2023-12-15 04:34:11 -0500

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,71 +1,93 @@
-"use client"
-import { Navbar, SimpleProjectContainer } from "../components";
+"use client";
+import { FooterContainer, Navbar, SimpleProjectContainer } from "../components";
 import { useEffect, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
-
+import { Anton } from "next/font/google";
 
 const images = ["/CCLP.png", "/DulcinaLanding1.png", "/vangSA.png"];
 
-const ProjectsPage = () => {
+const anton = Anton({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
 
-    useEffect(() => {
-        const lenis = new Lenis();
-    
-        function raf(time: any) {
-          lenis.raf(time);
-          requestAnimationFrame(raf);
-        }
-    
-        requestAnimationFrame(raf);
-      }, []);
+const ProjectsPage = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
-    <main className="h-[300rem]   text-[#ffffff]  bg-no-repeat bg-fixed ">
-      <div className="min-h-screen">
-        <Navbar/>
-        <div>
-          <div className="w-[100%] h-auto flex justify-center">
-            <div className="w-[100%] h-auto grid grid-cols-2 gap-x-4 gap-y-[15rem] justify-center">
-              <div className="h-[28rem] ">
-                <SimpleProjectContainer
-                  index={"01"}
-                  title="Dulcina"
-                  description="Chocolate gifts e-commerce"
-                  image={images[1]}
-                  bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
-                />
-              </div>
-              <div className="h-[28rem] ">
-                <SimpleProjectContainer
-                  index={"01"}
-                  title="Dulcina"
-                  description="Chocolate gifts e-commerce"
-                  image={images[1]}
-                  bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
-                />
-              </div>
-              <div className="h-[28rem] ">
-                <SimpleProjectContainer
-                  index={"01"}
-                  title="Dulcina"
-                  description="Chocolate gifts e-commerce"
-                  image={images[1]}
-                  bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
-                />
-              </div>
-              <div className="h-[28rem] ">
-                <SimpleProjectContainer
-                  index={"01"}
-                  title="Dulcina"
-                  description="Chocolate gifts e-commerce"
-                  image={images[1]}
-                  bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
-                />
+    <>
+      <main className="min-h-[100rem]   text-[#ffffff]  bg-no-repeat bg-fixed ">
+        <div className="min-h-[70rem]">
+          <Navbar />
+          <div className="flex flex-col justify-around items-center h-[50rem] mb-[10rem]">
+            <h2
+              className={`font-bold text-[3rem] sm:text-[16rem] ${anton.className} text-[#fff] `}
+            >
+              PROJECTS
+            </h2>
+            <div className=" font-light text-[3rem] sm:leading-[3.5rem] sm:text-[2.6rem] w-[90%] text-center">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="w-[100%] h-auto flex justify-center">
+              <div className="w-[100%] h-auto grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-[15rem] justify-center">
+                <div className="h-[28rem] ">
+                  <SimpleProjectContainer
+                    index={"01"}
+                    title="Dulcina"
+                    description="Chocolate gifts e-commerce"
+                    image={images[1]}
+                    bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
+                  />
+                </div>
+                <div className="h-[28rem] ">
+                  <SimpleProjectContainer
+                    index={"01"}
+                    title="Dulcina"
+                    description="Chocolate gifts e-commerce"
+                    image={images[1]}
+                    bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
+                  />
+                </div>
+                <div className="h-[28rem] ">
+                  <SimpleProjectContainer
+                    index={"01"}
+                    title="Dulcina"
+                    description="Chocolate gifts e-commerce"
+                    image={images[1]}
+                    bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
+                  />
+                </div>
+                <div className="h-[28rem] ">
+                  <SimpleProjectContainer
+                    index={"01"}
+                    title="Dulcina"
+                    description="Chocolate gifts e-commerce"
+                    image={images[1]}
+                    bgColor="bg-gradient-to-r from-[#403F3E] to-[#211917]"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+      <FooterContainer />
+    </>
   );
 };
 
