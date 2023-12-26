1417a49600badfe4f9950c14a5c48ae296762db4
David <davidalrojas@gmail.com>
2023-12-25 20:29:41 -0500

    fix props bug

--- src/app/components/ux/CAButton.tsx
+++ src/app/components/ux/CAButton.tsx
@@ -6,8 +6,7 @@ interface Props {
   content:string
 }
 
-const CAButton:FC<Props> = (props) => {
-  const {content} = props
+const CAButton:FC<Props> = ({content}) => {
 
   return (
     <button className=" relative flex items-center justify-between w-[100%] border-white border-solid border-[1px] h-[2.5rem] px-3">
