59b09305f9fbae5d2b58526ff70223cb6b406f2c
David <davidalrojas@gmail.com>
2023-12-28 19:37:04 -0500

    change language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,12 @@
 import {
   Alumni_Sans,
   Anton,
-  Barlow_Condensed,
-  Bebas_Neue,
-  Oswald,
 } from "next/font/google";
 import {
-  ButtonFooter,
-  CAButton,
-  CAText,
-  Card,
   Column,
   FooterContainer,
   Navbar,
-  SimpleProjectContainer,
-  SocialItems,
   TextCAContainer,
-  Title,
   WideProjects,
 } from "./components";
 import { useEffect, useRef } from "react";
@@ -45,7 +35,6 @@ export default function Home() {
     target: cont,
     offset: ["start end", "end start"],
   });
-  console.log(width);
   
 
   const y = useTransform(scrollYProgress, [0.2, 1.2], [0, height * 2]);
@@ -75,30 +64,31 @@ export default function Home() {
       <div className="min-h-screen">
         <Navbar />
         <div className=" flex flex-col pt-[4rem]  h-[100%] items-center justify-center">
-          <div className=" w-[90%] text-[4rem] leading-[4.5rem] sm:text-[9rem] sm:leading-[10rem]">
+          <div className=" w-[90%] text-[3.7rem] leading-[4.2rem] sm:text-[9rem] sm:leading-[10rem]">
             <h1
               className={` hidden sm:flex scale-x-200 scale-y-[1.5] ${Alumni.className}`}
             >
-              NOSOTROS CREAMOS
+              JUST A TAILOR-MADE
+
             </h1>
             <h1
               className={` sm:hidden scale-x-200 scale-y-[1.5] ${Alumni.className}`}
             >
-              NOSOTROS
+              JUST A
             </h1>
             <h1
               className={` sm:hidden scale-x-200 scale-y-[1.5] ${Alumni.className}`}
             >
-              CREAMOS
+              TAILOR-MADE 
             </h1>
             <h1 className={` scale-x-200 scale-y-[1.5] ${Alumni.className}`}>
-              LA PAGINA
+             WEBSITE
             </h1>
             <h1 className={` scale-x-200 scale-y-[1.5] ${Alumni.className}`}>
-              QUE NECESITAS
+              TO YOUR NEEDS
             </h1>
           </div>
-          <div className=" mt-[2rem] flex w-[90%] sm:w-[auto] sm:mt-0 sm:absolute sm:right-[4rem] sm:bottom-[10rem]">
+          <div className=" mt-[2rem] flex w-[90%] sm:w-[auto] sm:mt-0 sm:absolute sm:right-[4rem] sm:bottom-[11rem]">
             <TextCAContainer />
           </div>
         </div>
