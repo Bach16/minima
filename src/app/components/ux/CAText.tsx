93b9352f30e783ba0916069ec98d1d699f69c664
David <davidalrojas@gmail.com>
2023-12-29 12:48:54 -0500

    fix CAText bug

--- src/app/components/ux/CAText.tsx
+++ src/app/components/ux/CAText.tsx
@@ -5,7 +5,7 @@ const CAText = () => {
     <p className="w-[100%] sm:w-[30rem] text-[0.8rem] sm:text-[1rem] font-light">
       Transform your vision into a stunning online presence. Unlock the
       potential of your business with cutting-edge web solutions. Let{"'"}s craft a
-      unique online presence that stands out in the digital landscape—your
+      unique online presence that stands out in the digital landscape. Your
       journey to success begins here.
     </p>
   );
