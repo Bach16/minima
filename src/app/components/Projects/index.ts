d2bac910caed1c7208d61fe9ccf7f15a88f0cb07
Bach16 <gwyn56717@gmail.com>
2024-03-09 22:34:58 -0500

    move components folder

--- src/app/components/Projects/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Card } from './Card';
-export { default as SimpleProjectContainer } from './SimpleProjectContainer';
-export { default as Title } from './Title';
-export { default as WideProjects } from './WideProjects';
--- src/app/components/ux/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CAButton } from './CAButton';
-export { default as CAText } from './CAText';
-export { default as TextCAContainer } from './TextCAContainer';
