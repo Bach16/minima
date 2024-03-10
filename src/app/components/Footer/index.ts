be4f6c9b107b2ddbccaa1ae8a604ae2c4228ba63
Bach16 <gwyn56717@gmail.com>
2024-03-09 22:34:25 -0500

    move components folder

--- src/app/components/Footer/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ButtonFooter } from './ButtonFooter';
-export { default as FooterContainer } from './FooterContainer';
-export { default as SocialItems } from './SocialItems';
--- src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Column } from './Column';
-export * from './Footer';
-export { default as Navbar } from './Navbar';
-export * from './Projects';
-export * from './ux';
