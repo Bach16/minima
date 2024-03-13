bcb1cea1040f868446e8614ec1f5e8afe865767e
Bach16 <gwyn56717@gmail.com>
2024-03-13 18:57:23 -0500

    add google analytics tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider, useMessages } from "next-intl";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,21 @@ const RootLayout: React.FC<Props> = ({ children, params: { locale } }) => {
   const messages = useMessages();
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R51DX82E18"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R51DX82E18');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} bg-black max-w-[200rem] h-auto`}>
         <NextIntlClientProvider messages={messages}>
           {children}
