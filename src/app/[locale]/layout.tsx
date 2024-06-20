92fdf24d4b24078115f1f2c4a39b756c9225a5d7
Bach16 <gwyn56717@gmail.com>
2024-06-19 20:24:40 -0500

    add clarity tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,6 +46,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             gtag('config', 'G-R51DX82E18');
           `}
         </Script>
+        <Script type="text/javascript" id="microsoft-clarity">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "munrfoh3a4");
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} bg-black max-w-[200rem] h-auto`}>
         <noscript>
